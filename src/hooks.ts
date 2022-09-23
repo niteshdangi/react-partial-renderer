5c158eb50499a0b958353a8afab42e549a164c48
Nitesh Kumar <niteshdangi28@gmail.com>
2022-09-24 00:37:14 +0530

    initial commit

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,14 +3,14 @@ import { useEffect, useState } from 'react'
 export const useReadyRenderHelper = (delay = 0, shouldWait = false, onReady: (() => void) | undefined = undefined) => {
   const [ready, setReady] = useState(false)
   useEffect(() => {
-    let timeout: NodeJS.Timeout
     if (!shouldWait) {
-      timeout = setTimeout(() => {
+      const timeout = setTimeout(() => {
         setReady(true)
         onReady?.()
       }, delay || 0)
+      return () => clearTimeout(timeout)
     }
-    return () => clearTimeout(timeout)
+    return () => {}
   }, [shouldWait, delay, onReady])
   return ready
 }
